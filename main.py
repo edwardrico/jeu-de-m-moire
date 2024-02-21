9498f5e0e8954b1e3586bdaeb9362bde6c88a15f
edwardrico <120380332+edwardrico@users.noreply.github.com>
2024-02-21 19:17:33 +0100

    sequence

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ import random
 import os
 
 
+# function pour netoiyer l'ecran
+def clear_screen():
+    if os.name == "posix":
+        os.system("clear")
+    else:
+        os.system("cls")
+
+
+def sequence_initiale():
+    sequence = ""
+    for i in range(4):
+        chiffre = random.randint(0, 9)
+        sequence += str(chiffre)
+
