92252a8068b47663f1d7e006d5c0f5e217da62c6
edwardrico <120380332+edwardrico@users.noreply.github.com>
2024-02-22 17:40:55 +0100

    correction

--- main.py
+++ main.py
@@ -25,16 +25,16 @@ def main():
     sequence = sequence_initiale()
     while True:
         print("Memorise la sequence ")
-        print(sequence)
+        print(f"{sequence}\n")
         time.sleep(3)
         clear_screen()
-        entree = input("Quelle est la sequence : ")
+        entree = input("Entrez la séquence:\n")
         if entree == sequence:
-            print("Bravo vous avez reusir")
+            print("Bravo, vous avez réussi \n")
             sequence += str(random.randint(0, 9))
             point += 1
         else:
-            print(f" Vous avez perdu, vous cumulez {point} point et la sequence est {sequence}")
+            print(f"Vous avez perdu. Votre score est de {point} points et la séquence était {sequence}\n")
             break
 
 
