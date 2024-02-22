51955034eb169e92998046d5bb2d05cc00533919
edwardrico <120380332+edwardrico@users.noreply.github.com>
2024-02-22 14:53:11 +0100

    termine

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import os
 
 
-# function pour netoiyer l'ecran
+# Fonction pour nettoyer l'écran
 def clear_screen():
     if os.name == "posix":
         os.system("clear")
@@ -17,3 +17,26 @@ def sequence_initiale():
         chiffre = random.randint(0, 9)
         sequence += str(chiffre)
 
+    return sequence
+
+
+def main():
+    point = 0
+    sequence = sequence_initiale()
+    while True:
+        print("Memorise la sequence ")
+        print(sequence)
+        time.sleep(3)
+        clear_screen()
+        entree = input("Quelle est la sequence : ")
+        if entree == sequence:
+            print("Bravo vous avez reusir")
+            sequence += str(random.randint(0, 9))
+            point += 1
+        else:
+            print(f" Vous avez perdu, vous cumulez {point} point et la sequence est {sequence}")
+            break
+
+
+if __name__ == "__main__":
+    main()
